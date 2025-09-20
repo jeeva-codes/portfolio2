17d8d68daced09e1e9c32b7ac6234639b96365cc
jeeva-codes <jeevacode2@gmail.com>
2025-09-19 23:14:41 -0800

    changes

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -14,7 +14,7 @@ function Hero() {
       animate={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.6, ease: "easeOut" }} className="text-2xl">React Developer</motion.h1>
       <div className="mt-5 mb-5">
-        <img src="/1754115403967.jpg"  alt="" className="size-40 m-auto rounded-full" />
+        <img src="/img/js_5968292.png"  alt="" className="size-40 m-auto rounded-full" />
       </div>
       <p className="w-80 m-auto mb-8 text-1xl">A passionate React developer eager to create engaging web experiences</p>
         <div className="flex justify-center">
@@ -27,7 +27,7 @@ function Hero() {
             <a href="/Jeeva-Reactdev-Resume.pdf" download>
                  <button className="p-2 text-1xl font-semibold text-black rounded-3xl bg-green-400  ml-3" data-aos="fade-up"  data-aos-delay="150"
     data-aos-duration="1000"
-    data-aos-easing="ease-in-out">DownloadResume</button>
+    data-aos-easing="ease-in-out">Download Resume</button>
             </a>
            
         </div>
